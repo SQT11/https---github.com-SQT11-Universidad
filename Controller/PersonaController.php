6a64d8c7ba16b6a065bb5ea10bd163bf2969ef6b
SQT11 <ensantyquintero@gmail.com>
2023-06-23 09:18:49 -0500

    Cambios en persona y elimancion en cascada

---
+++ Controller/PersonaController.php
@@ -0,0 +1,83 @@
+<?php
+/*
+require_once ('../core/config.php');
+require_once ('../Models/PesonaModel.php');
+*/
+$rutaCarpeta = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$rutaProyecto = explode("/", $rutaCarpeta);
+
+require_once $_SERVER['DOCUMENT_ROOT']. "/" . $rutaProyecto[1] .'/Models/PersonaModel.php';
+require_once $_SERVER['DOCUMENT_ROOT']. "/" . $rutaProyecto[1] .'/core/config.php';
+
+class PersonaController {
+    private $personaModel;
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+        $this->personaModel = new PersonaModel($this->conn);
+    }
+
+    public function agregarPersona($data) {
+        $nombres = $data['nombre'];
+        $documentos = $data['documento'];
+        $correos = $data['correo'];
+        $telefonos = $data['telefono'];
+        $opciones = implode(",", $data['opcion']);
+
+        if ($this->personaModel->insertData($nombres, $documentos, $correos, $telefonos, $opciones)) {
+            echo "Datos agregados";
+            header("Location: ../views/Persona.php");
+            exit; // Asegura que el script se detenga después de redireccionar
+        } else {
+            echo "Error al insertar datos";
+        }
+        
+    }
+
+    public function actualizarPersona($data) {
+        $idpersona = $data['idpersona'];
+        $nombres = $data['nombre'];
+        $documentos = $data['documento'];
+        $correos = $data['correo'];
+        $telefonos = $data['telefono'];
+        $opciones = implode(",", $data['opcion']);
+
+        if ($this->personaModel->updateData($idpersona, $nombres, $documentos, $correos, $telefonos, $opciones)) {
+            echo "Datos actualizados";
+            header("Location: ../views/Persona.php");
+            exit; // Asegura que el script se detenga después de redireccionar
+        } else {
+            echo "Error al actualizar datos";
+        }
+    }
+
+    public function eliminarPersona($idpersona) {
+        if ($this->personaModel->deleteData($idpersona)) {
+            echo "Registro eliminado";
+            header("Location: ../views/Persona.php");
+            exit; // Asegura que el script se detenga después de redireccionar
+        } else {
+            echo "Error al eliminar registro";
+        }
+    }
+
+    public function obtenerPersonas() {
+        return $this->personaModel->getAllData();
+    }
+}
+
+$Persona = new PersonaController($conn);
+if ($_POST['agregar']) {
+    $Persona->agregarPersona($_POST);
+}
+
+else if ($_POST['actualizar']) {
+    $Persona->actualizarPersona($_POST);
+}
+
+else if ($_POST['eliminar']) {
+    $Persona->eliminarPersona($_POST);
+}
+?>
+
